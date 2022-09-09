3f74c01172c2c9e47bbcdcbbe9997bd9d6f8e2e5
wealthlaw <victorlawrence16@gmail.com>
2022-09-09 11:21:25 -0700

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+	printf("\n");
+
+	return (0);
+}
