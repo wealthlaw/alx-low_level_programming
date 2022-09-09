72b169ef796eada847758f124a11c6be070eafed
wealthlaw <victorlawrence16@gmail.com>
2022-09-09 11:38:06 -0700

    charnums

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+{
+	putchar (i);
+}
+	putchar ('\n');
+
+	return (0);
+}
