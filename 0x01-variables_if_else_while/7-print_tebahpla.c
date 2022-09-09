bc6296d7cd6905857f03fda18a5a3dd5e37fec00
wealthlaw <victorlawrence16@gmail.com>
2022-09-09 11:50:10 -0700

    7done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets reverse
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char A;
+
+	for (A = 122; A >= 97; A--)
+{
+	putchar (A);
+}
+	putchar ('\n');
+	return (0);
+}
