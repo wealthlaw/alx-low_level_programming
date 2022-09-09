28fda7e12009ba9ff5e682028d462d4ad365dc35
wealthlaw <victorlawrence16@gmail.com>
2022-09-09 07:02:36 -0700

    final betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints text values
- * return: Always 0 (success)
+ * main - Prints a statement
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	puts("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Print sizes
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Print sizes
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char a;
@@ -18,12 +13,12 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
-        
+
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 
-      	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 
 	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
