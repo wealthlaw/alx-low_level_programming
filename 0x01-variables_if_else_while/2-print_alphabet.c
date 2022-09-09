be85ce0b03e333409f3d01dace0f72dc5aa8f358
wealthlaw <victorlawrence16@gmail.com>
2022-09-09 10:21:48 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+{
+	putchar(A);
+}
+	putchar('\n');
+
+	return (0);
+}
