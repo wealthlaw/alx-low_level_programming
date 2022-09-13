1e23d621c582964ba96f83511324fbccb72e55ab
wealthlaw <victorlawrence16@gmail.com>
2022-09-13 08:36:34 -0700

    4 alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet
+ * @c: - function argument
+ * Return: 1 or 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c >= 'Z')
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
