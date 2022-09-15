ac381b5ab6b86564d9975d9b4d304b4816034713
wealthlaw <victorlawrence16@gmail.com>
2022-09-15 10:59:39 -0700

    4task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints all numbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
