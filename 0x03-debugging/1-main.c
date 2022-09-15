efc70412d49d082b4caf6be0be31e7c229c8c148
wealthlaw <victorlawrence16@gmail.com>
2022-09-15 07:57:24 -0700

    checker not fully showing

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-	
 	int i;
 
 	printf("Infinite loop incoming :(\n");
@@ -19,7 +18,6 @@ int main(void)
  * putchar(i);
  * }
  */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
