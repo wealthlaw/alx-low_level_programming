bb1112c7f7ea770732830940a25c2f5eea5171d3
wealthlaw <victorlawrence16@gmail.com>
2022-09-23 17:17:03 -0700

    1-4

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - C function that copies a string.
+ * @dest: temporary storage for string copy
+ * @src:the source string
+ * @n:max nummber of byte copied
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
