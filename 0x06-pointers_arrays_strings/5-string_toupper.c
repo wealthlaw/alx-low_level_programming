3e8241da806f974166bbf575696fccbad006002f
wealthlaw <victorlawrence16@gmail.com>
2022-09-23 18:06:32 -0700

    5-7

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase string to uppercase
+ * @str: string
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
