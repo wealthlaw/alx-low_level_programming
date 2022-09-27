24c38bf53451e33e8d6589c663b55b1015df43f0
wealthlaw <victorlawrence16@gmail.com>
2022-09-27 12:10:59 -0700

    string charcter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - fins a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer to c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+{
+	if (s[i] == c)
+	return (s + i);
+}
+	if (s[i] == c)
+	return (s + i)
+	return (0);
+}
