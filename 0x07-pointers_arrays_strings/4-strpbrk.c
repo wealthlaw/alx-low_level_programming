4fde018dbb4ea1f2416641f59dacded286d95fd8
wealthlaw <victorlawrence16@gmail.com>
2022-09-27 13:02:09 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches string for any set of bytes
+ * @s: string 
+ * @accept: string to compare
+ *
+ * Return: pointer to byte in s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+{
+	for (j = 0; accept[j]; j++)
+{
+	if (s[i] == accept[j])
+	break;
+}
+	if (accept[j])
+	return (s + i);
+}
+	return (0);
+}
