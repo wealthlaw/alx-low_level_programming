ecd62e66d950943d44f5084e34ecc518afbd7d20
wealthlaw <victorlawrence16@gmail.com>
2022-09-30 16:10:17 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/what.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+{
+	printf("%i", i);
+	printf("\n");
+}
+return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Starting to God in c
